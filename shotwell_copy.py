ca1bac35f160f6f10abb1e875e4e61fcb33519a3
Sameer Morar <smorar@gmail.com>
2020-02-13 00:31:34 +1000

    * Copy across exposure_time, orientation, import_id. Copy event groupings. * Open input database as read-only

--- shotwell_copy.py
+++ shotwell_copy.py
@@ -8,7 +8,7 @@ def initialize_output_database(output_file):
     # Create new sqlite database to store exported data
     conn = sqlite3.connect(output_file)
     c = conn.cursor()
-    c.execute('''
+    sql_create_phototable = """
     CREATE TABLE PhotoTable 
     (id INTEGER PRIMARY KEY, 
     filename TEXT UNIQUE NOT NULL, 
@@ -39,14 +39,28 @@ def initialize_output_database(output_file):
     develop_camera_id INTEGER DEFAULT -1, 
     develop_embedded_id INTEGER DEFAULT -1, 
     comment TEXT)
-    ''')
+    """
+    c.execute(sql_create_phototable)
+    conn.commit()
+    sql_create_eventtable = """
+    CREATE TABLE EventTable 
+    (id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    primary_photo_id INTEGER, 
+    time_created INTEGER,
+    primary_source_id TEXT,
+    comment TEXT)
+    """
+    c.execute(sql_create_eventtable)
     conn.commit()
     conn.close()
 
 def process_databases(input_file, output_file):
-    conn_ifile = sqlite3.connect(input_file)
+    ifile_uri = ('file:{0}?mode=ro').format(input_file)
+    conn_ifile = sqlite3.connect(ifile_uri, uri=True)
     conn_ofile = sqlite3.connect(output_file)
     c_ifile = conn_ifile.cursor()
+    c_ifile2 = conn_ifile.cursor()
     c_ofile = conn_ofile.cursor()
 
     sql_select_transformations = "SELECT * FROM PhotoTable WHERE transformations IS NOT NULL"
@@ -90,6 +104,53 @@ def process_databases(input_file, output_file):
             sql_copy_record = ("INSERT INTO PhotoTable VALUES {0}").format(record_str)
             c_ofile.execute(sql_copy_record)
             conn_ofile.commit()
+    
+    processed_inputeventids = []
+    sql_iterate_itable = "SELECT filename, exposure_time, orientation, import_id, event_id FROM PhotoTable"
+    for row in c_ifile.execute(sql_iterate_itable):
+        sql_record_exists = ('SELECT EXISTS(SELECT 1 FROM PhotoTable WHERE filename="{0}")').format(row[0])
+        c_ofile.execute(sql_record_exists)
+        if ((c_ofile.fetchone()[0]) == 1):
+            print("matched 1")
+            # matched file in input and output database
+            sql_update_data = ('UPDATE PhotoTable SET exposure_time={0}, orientation={1}, import_id={2} WHERE filename="{3}"').format(row[1], row[2], row[3], row[0])
+            c_ofile.execute(sql_update_data)
+            conn_ofile.commit()
+            sql_get_current_oevent_id = ('SELECT event_id FROM PhotoTable WHERE filename="{0}"').format(row[0])
+            c_ofile.execute(sql_get_current_oevent_id)
+            current_oevent_id = c_ofile.fetchone()[0]
+            # TODO - group photos by event
+            if processed_inputeventids.count(row[4]) == 0:
+                processed_inputeventids.append(row[4])
+                sql_get_input_event = ('SELECT * FROM EventTable WHERE id={0}').format(row[4])
+                c_ifile2.execute(sql_get_input_event)
+                event_data = c_ifile2.fetchone()
+                sql_check_output_event = ('SELECT EXISTS(SELECT 1 FROM EventTable WHERE time_created={0})').format(event_data[3])
+                c_ofile.execute(sql_check_output_event)
+                if ((c_ofile.fetchone()[0]) == 1):
+                    # Event exists. Update all pictures with this event id
+                    sql_get_output_eventid = ('SELECT id FROM EventTable WHERE time_created={0}').format(event_data[3])
+                    c_ofile.execute(sql_get_output_eventid)
+                    output_eventid = c_ofile.fetchone()[0]
+                    sql_update_event_id = ('UPDATE PhotoTable SET event_id={0} WHERE event_id={1}').format(output_eventid, current_oevent_id)
+                    c_ofile.execute(sql_update_event_id)
+                    conn_ofile.commit()
+                else:
+                    # Event doesn't exist. Create and then update all pictures with created id
+                    print("event doesn't exist")
+                    event = list(event_data)
+                    event[0] = None
+                    event_str = tuple(event).__str__()
+                    event_str = event_str.replace("None", "NULL")
+                    sql_copy_event = ("INSERT INTO EventTable VALUES {0}").format(event_str)
+                    c_ofile.execute(sql_copy_event)
+                    conn_ofile.commit()
+                    sql_obtain_id = "SELECT last_insert_rowid()"
+                    c_ofile.execute(sql_obtain_id)
+                    output_eventid = c_ofile.fetchone()[0]
+                    sql_update_event_id = ('UPDATE PhotoTable SET event_id={0} WHERE event_id={1}').format(output_eventid, current_oevent_id)
+                    c_ofile.execute(sql_update_event_id)
+                    conn_ofile.commit()
 
     conn_ifile.close()
     conn_ofile.close()
